78cecffc1c4cf11eb909004be8cf36a7dba315f1
Keith Smiley <keithbodesmiley@gmail.com>
2012-06-26 11:13:58 -0400

    Init

--- ShortcutRecorderTest/KSAppDelegate.h
+++ ShortcutRecorderTest/KSAppDelegate.h
@@ -7,9 +7,24 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <ShortcutRecorder/ShortcutRecorder.h>
+#import "PTHotKey.h"
+#import "PTHotKeyCenter.h"
 
-@interface KSAppDelegate : NSObject <NSApplicationDelegate>
+@interface KSAppDelegate : NSObject <NSApplicationDelegate> {
+    IBOutlet SRRecorderControl *shortcutRecorder;
+    IBOutlet SRRecorderControl *shortcutRecorderTwo;
+    
+    PTHotKey *globalHotKey;
+    PTKeyCombo *akeyCombo;
+    PTHotKey *hotKey;
+    PTHotKey *otherHotKey;
+    PTHotKeyCenter *hotKeyCenter;
+    NSUserDefaults *userDefaults;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
++ (BOOL)universalAccessNeedsToBeTurnedOn;
 
 @end
--- ShortcutRecorderTest/KSAppDelegate.m
+++ ShortcutRecorderTest/KSAppDelegate.m
@@ -14,7 +14,106 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [KSAppDelegate universalAccessNeedsToBeTurnedOn];
+    userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    [shortcutRecorder setCanCaptureGlobalHotKeys:YES];
+    [shortcutRecorderTwo setCanCaptureGlobalHotKeys:YES];
+    
+    id firstKey = [userDefaults objectForKey:@"hi"];
+    PTKeyCombo *keys = [[PTKeyCombo alloc] initWithPlistRepresentation:firstKey];
+    
+    KeyCombo someKeyCombo = SRMakeKeyCombo([keys keyCode], SRCarbonToCocoaFlags([keys modifiers]));
+    [shortcutRecorder setKeyCombo:someKeyCombo];
+    
+    id secondKey = [userDefaults objectForKey:@"hello"];
+    keys = [[PTKeyCombo alloc] initWithPlistRepresentation:secondKey];
+    someKeyCombo = SRMakeKeyCombo([keys keyCode], SRCarbonToCocoaFlags([keys modifiers]));
+    [shortcutRecorderTwo setKeyCombo:someKeyCombo];
+
+}
+
+- (void)sayHI {
+    NSLog(@"hiff");
+}
+
+- (void)sayBye {
+    NSLog(@"byee");
+}
+
+- (BOOL)shortcutRecorder:(SRRecorderControl *)aRecorder isKeyCode:(NSInteger)keyCode andFlagsTaken:(NSUInteger)flags reason:(NSString **)aReason {
+    KeyCombo kc;
+    BOOL isTaken = NO;
+	if (aRecorder == shortcutRecorder) {
+		kc = [shortcutRecorderTwo keyCombo];
+	} else if (aRecorder == shortcutRecorderTwo) {
+        kc = [shortcutRecorder keyCombo];
+    }
+    if (kc.code == keyCode && kc.flags == flags) {
+        isTaken = YES;
+        *aReason = @"it's already in use by SRTest";
+        return isTaken;
+    }
+    
+	return NO;
+}
+
+- (void)shortcutRecorder:(SRRecorderControl *)aRecorder keyComboDidChange:(KeyCombo)newKeyCombo {
+    userDefaults = [NSUserDefaults standardUserDefaults];
+    hotKeyCenter = [PTHotKeyCenter sharedCenter];
+    
+    signed short code = newKeyCombo.code;
+    unsigned int flags = [aRecorder cocoaToCarbonFlags:newKeyCombo.flags];
+    akeyCombo = [[PTKeyCombo alloc] initWithKeyCode:code modifiers:flags];
+    
+    if (aRecorder == shortcutRecorder) {
+        id firstKey = [userDefaults objectForKey:@"hi"];
+        [hotKeyCenter unregisterHotKey:otherHotKey]; // The Key to happiness
+        otherHotKey = nil;
+        
+        otherHotKey = [[PTHotKey alloc] initWithIdentifier:firstKey keyCombo:akeyCombo];
+        [userDefaults setObject:[akeyCombo plistRepresentation] forKey:@"hi"];
+        [otherHotKey setTarget:self];
+        [otherHotKey setAction:@selector(sayBye)];
+        [hotKeyCenter registerHotKey:otherHotKey];
+    } else if (aRecorder == shortcutRecorderTwo) {
+        id secondKey = [userDefaults objectForKey:@"hello"];
+        [hotKeyCenter unregisterHotKey:hotKey];
+        hotKey = nil;
+        
+        hotKey = [[PTHotKey alloc] initWithIdentifier:secondKey keyCombo:akeyCombo];
+        [userDefaults setObject:[akeyCombo plistRepresentation] forKey:@"hello"];
+        [hotKey setTarget:self];
+        [hotKey setAction:@selector(sayHI)];
+        [hotKeyCenter registerHotKey:hotKey];
+    }
+    
+    
+//    if (newKeyCombo.code == ShortcutRecorderEmptyCode & newKeyCombo.flags == ShortcutRecorderEmptyFlags) {
+//    }
+    
+    [userDefaults synchronize];
+}
+
+
++ (BOOL)universalAccessNeedsToBeTurnedOn {
+    if (!AXAPIEnabled()) {
+        NSString *message = NSLocalizedString(@"QuickCursor requires that you launch the Universal Access preferences pane and turn on \"Enable access for assistive devices\".", nil);
+        NSUInteger result = NSRunAlertPanel(message, @"", NSLocalizedString(@"OK", nil), NSLocalizedString(@"Quit QuickCursor", nil), NSLocalizedString(@"Cancel", nil));
+        
+        switch (result) {
+            case NSAlertDefaultReturn:
+                [[NSWorkspace sharedWorkspace] openFile:@"/System/Library/PreferencePanes/UniversalAccessPref.prefPane"];
+                break;
+                
+            case NSAlertAlternateReturn:
+                [NSApp terminate:self];
+                break;
+        }
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 @end
