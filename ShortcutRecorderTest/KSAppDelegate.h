85201e2841605e299ad1abc5a1b5a2da3824a2cf
Keith Smiley <keithbodesmiley@gmail.com>
2012-06-26 11:02:23 -0400

    Initial Commit

---
+++ ShortcutRecorderTest/KSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KSAppDelegate.h
+//  ShortcutRecorderTest
+//
+//  Created by Keith Smiley on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ShortcutRecorderTest/KSAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  KSAppDelegate.m
+//  ShortcutRecorderTest
+//
+//  Created by Keith Smiley on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KSAppDelegate.h"
+
+@implementation KSAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
